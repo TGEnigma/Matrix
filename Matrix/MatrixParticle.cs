ab4f13d4c1a368c09086855dc14823bf964c775b
TGEnigma <smartychrisje@hotmail.com>
2017-09-13 18:04:29 +0200

    Initial commit

---
+++ Matrix/MatrixParticle.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Matrix
+{
+    public class MatrixParticle
+    {
+        public static Random Random = new Random();
+
+        private int mTrailLength;
+        private bool mVisible = true;
+
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public MatrixParticle( int startingX, int trailLength )
+        {
+            // Starting x value will be variable but we'll always start the top of the window
+            X = startingX;
+            Y = Console.BufferHeight - 1;
+
+            mTrailLength = trailLength;
+        }
+
+        public void Update()
+        {
+            // Check if we're nearing the end of the screen
+            if ( Y == 1 )
+            {
+                if ( mTrailLength != 0 )
+                {
+                    // Decrease trail length and increase Y of current particle
+                    // to fake a smooth transition to nothing effect
+                    mTrailLength--;
+                    Y += 1;
+                }
+            }
+
+            // Down we go
+            Y--;
+
+            if ( Y == 0 )
+            {
+                // Goodbye forever particle
+                mVisible = false;
+            }
+
+            //Debug.WriteLine( $"[{X}, {Y}]" );
+        }
+
+        public void Draw()
+        {
+            if ( !mVisible )
+            {
+                // Erase the particle
+                for ( int i = 0; i < mTrailLength + 1; i++ )
+                {
+                    int charY = i + 1;
+
+                    if ( charY < Console.BufferHeight )
+                        WriteCharacter( X, Y + ( i + 1 ), ' ' );
+                }             
+            }
+            else
+            {
+                // Hack way to clear the last character
+                int lastCharY = Y + mTrailLength + 1;
+
+                if ( lastCharY < Console.BufferHeight )
+                {
+                    WriteCharacter( X, lastCharY, ' ' );
+                }
+
+                // Draw the trailing particles
+                for ( int i = 0; i < mTrailLength; i++ )
+                {
+                    int trailY = Y + ( i + 1 );
+
+                    if ( trailY < Console.BufferHeight )
+                    {
+                        WriteRandomASCIICharacter( X, trailY, Random.Next( 0, 2 ) == 0 ? ConsoleColor.Green : ConsoleColor.DarkGreen );
+                    }
+                    else
+                    {
+                        // If we're already at the top we can stop trying
+                        break;
+                    }
+                }
+
+                WriteRandomASCIICharacter( X, Y, ConsoleColor.White );
+            }
+        }
+
+        private static void WriteCharacter( int x, int y, char character )
+        {
+            Console.SetCursorPosition( x, ( Console.BufferHeight - 1 ) - y );
+            Console.Write( character );
+        }
+
+        private static void WriteRandomASCIICharacter( int x, int y, ConsoleColor color )
+        {
+            Console.SetCursorPosition( x, ( Console.BufferHeight - 1 ) - y );
+            Console.ForegroundColor = color;
+            Console.Write( (char)Random.Next( 32, 127 ) );
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        private static Random Random = new Random();
+
+        public static float FPS { get; set; } = 60;
+
+        public static int ParticleEmitFrequency { get; set; } = 1;
+
+        public static int ParticleEmitCount { get; set; } = 1;
+
+        public static int ParticleMinTrailLength { get; set; } = 2;
+
+        public static int ParticleMaxTrailLength { get; set; } = 10;
+
+        private static void Main(string[] args)
+        {
+            // update params
+            int updateFrequency = ( int )( ( 1 / FPS ) * 1000 );
+            var stopwatch = new Stopwatch();
+
+            // render params
+            Console.BufferHeight = 30;
+
+            // ye good ol particles
+            var particles = new List<MatrixParticle>();
+            //particles.Add( new MatrixParticle( Random.Next( 0, Console.BufferWidth ) ), 5 );       
+
+            // 'game' loop
+            int frameCount = 0;
+            while (true)
+            {
+                stopwatch.Reset();
+
+                // Non blocking input
+                if ( Console.KeyAvailable )
+                {
+                    var key = Console.ReadKey();
+                    if ( key.Key == ConsoleKey.UpArrow )
+                    {
+                        ++ParticleEmitCount;
+                    }
+                    else if ( key.Key == ConsoleKey.DownArrow )
+                    {
+                        if ( ParticleEmitCount > 0 )
+                            --ParticleEmitCount;
+                    }
+                }
+
+                // make some particles, maybe
+                if ( (frameCount % ParticleEmitFrequency) == 0 )
+                {
+                    for ( int i = 0; i < ParticleEmitCount; i++ )
+                        particles.Add( CreateRandomParticle() );
+                }                   
+
+                // render & update particles
+                int removedParticleCount = 0;
+                for ( int i = ( 0 - removedParticleCount ); i < ( particles.Count - removedParticleCount ); i++ )
+                {
+                    var particle = particles[i];
+
+                    // Remove particles on the bottom of the screen
+                    if ( particle.Y == 0 )
+                    {
+                        particles.Remove( particle );
+                        ++removedParticleCount;
+                        continue;
+                    }
+
+                    particle.Update();
+                    particle.Draw();
+                }
+                
+                // try to maintain target frame rate
+                // using a variable time step would be better but that's a lot of effort
+                long timeSpent = stopwatch.ElapsedMilliseconds;
+                int sleepTime = ( int )( updateFrequency - timeSpent );
+                if ( sleepTime > 0 )
+                {
+                    Thread.Sleep( sleepTime );
+                }
+
+                ++frameCount;
+            }
+        }
+
+        private static MatrixParticle CreateRandomParticle()
+        {
+            return new MatrixParticle( Random.Next( 0, Console.BufferWidth ), Random.Next( ParticleMaxTrailLength, ParticleMaxTrailLength + 1 ) );
+        }
+    }
+}
